a7150a49a75dcff240aa753700fb8a9415c5d576
Jed Stone <jstone31@student.wiregrass.edu>
2022-05-16 08:43:50 -0400

    first

---
+++ class day one/class day one/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace class_day_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
