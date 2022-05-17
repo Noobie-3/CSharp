f8884b80d190afb3d97d07889a3d83960c2b268f
Jed Stone <jstone31@student.wiregrass.edu>
2022-05-17 08:22:25 -0400

    windform

---
+++ day one class Csharp/day one class Csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace day_one_class_Csharp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+    }
+
+}
