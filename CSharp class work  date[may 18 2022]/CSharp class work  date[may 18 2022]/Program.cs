e2cf3c27a31e46bfde6c6fd0b29883d7085ec6f8
Jed Stone <jstone31@student.wiregrass.edu>
2022-05-19 11:36:14 -0400

    First homework

---
+++ CSharp class work  date[may 18 2022]/CSharp class work  date[may 18 2022]/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp_class_work__date_may_18_2022_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+ 
---
+++ CSharpo Homeworko 1/CSharpo Homeworko 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpo_Homeworko_1
+{
+    class Program {
+        static void Main(string[] args) {    
+
+            //takes in a number
+            Console.WriteLine("Please enter a Number that will be worked with\t");
+            string Number = Console.ReadLine();
+            int numconverted = Int32.Parse(Number);
+            
+            //Writed the new numbers 
+            Console.WriteLine("Number Squared: " + (numconverted * numconverted));
+            Console.WriteLine("Number devided by 2: " + (numconverted / 2));
+            Console.WriteLine("Number minis 100: " + (numconverted - 100));
+        }
+    }
+}
